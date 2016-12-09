8ca7f61ede24a0be7f04361a16017cd957b8b5dd
Andrew White <aeewhite@gmail.com>
2016-12-09 00:47:00 -0600

    Move language errors into an exception

--- src/main/java/em/EmException.java
+++ src/main/java/em/EmException.java
@@ -1,8 +1,70 @@
 package em;
 
+import java.io.File;
+import java.nio.file.Files;
+
 /**
  * Created by andrew on 11/24/16.
  */
 public class EmException extends RuntimeException {
+    public EmException(String message, Lexeme lexeme){
+        //Top line
+        String errorLine;
+        if(lexeme.getFile() != null){
+            errorLine = String.format("ERROR in %s at (%d,%d)",
+                    lexeme.getFile().getName(),
+                    lexeme.getRow(),
+                    lexeme.getCol());
+            String fileline = readLineFromFile(lexeme.getFile(), lexeme.getRow());
+            int tabCount = fileline.length() - fileline.replace("\t", "").length();
+            String caretline = leftpadcaret(tabCount, lexeme.getCol() - tabCount -1, "^");
+            Logger.error(String.format("%s\n\t%s\n\t%s\n\t%s",
+                    errorLine,
+                    fileline,
+                    caretline,
+                    message));
+        }
+        else{
+            Logger.error("ERROR: " + message);
+        }
+        //Throw fatal error
+        System.exit(1);
+    }
 
+    /**
+     * Read a single line from a file
+     * (line numbers start at 1)
+     * @param file
+     * @param linenumber
+     * @return line from file as a String
+     */
+    private String readLineFromFile(File file, int linenumber){
+        try{
+            return Files.readAllLines(file.toPath()).get(linenumber - 1);
+        }
+        catch (Exception e){
+            return "ERROR";
+        }
+    }
+
+    /**
+     * Create a string with spaces to the left of a character
+     * @param tabs Number of tabs to pad
+     * @param spaces Number of spaces to pad
+     * @param start Character to place on the right
+     * @return
+     */
+    private String leftpadcaret(int tabs, int spaces, String start){
+        if(tabs <= 0 && spaces <= 0){
+            return start;
+        }
+        else if(spaces > 0 ){
+            return leftpadcaret(tabs, --spaces, " "  + start);
+        }
+
+        else{
+            // (tabs > 0)
+            return leftpadcaret(--tabs, spaces, "\t"  + start);
+        }
+    }
 }
