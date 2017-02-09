bd8dd3545e6f7e1d5cb2b1cf5efa84101722f60e
Andrew White <aeewhite@gmail.com>
2017-02-09 14:17:24 -0600

    Get command line options working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world! lol");
+	// Setup the command line interface
+	let matches = App::new("Em")
+						.version("2.0.0")
+						// .about("An interpreted, dynamically typed language")
+						.arg(Arg::with_name("verbose")
+								.short("v")
+								.help("Set the verbosity level")
+								.multiple(true))
+						.arg(Arg::with_name("file")
+								.required(true))
+						.get_matches();
+	
+	//Determining the verbosity
+	let verbosity: u64 = matches.occurrences_of("verbose");
+	if matches.occurrences_of("verbose") > 0 {
+		println!("verbosity level: {}", matches.occurrences_of("verbose"));
+	}
+	
+
+	println!("Hello, world! lol");
 }
